8f424d681193fcc6eef92879ad3492a0b55788af
thor1824 <42994659+thor1824@users.noreply.github.com>
2018-11-14 12:15:31 +0100

    peter changes

--- src/movierecsys/dal/RatingDAO.java
+++ src/movierecsys/dal/RatingDAO.java
@@ -11,14 +11,15 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import movierecsys.be.Movie;
 import movierecsys.be.Rating;
 import movierecsys.be.User;
 
@@ -27,9 +28,9 @@ import movierecsys.be.User;
  * @author pgn
  */
 public class RatingDAO {
-
-    private static final String RATING_SOURCE = "data/ratings.txt";
     private static final String TEMP_SOURCE = "data/temp.txt";
+    private static final String RATING_SOURCE = "data/user_ratings";
+    private static final int RECORD_SIZE = Integer.BYTES * 3;
 
     /**
      * Persists the given rating.
@@ -50,28 +51,52 @@ public class RatingDAO {
         return rating;
     }
 
-    /**
+    /**peter
      * Updates the rating to reflect the given object.
      *
      * @param newRating The updated rating to persist.
      * @throws java.io.IOException
      */
-    public void updateRating(Rating newRating) throws IOException {
-        File tmp = new File(RATING_SOURCE);
-        List<Rating> allRating = getAllRatings();
-        allRating.removeIf((Rating v) -> v.getMovie().getId() == newRating.getMovie().getId());
-        allRating.add(newRating);
-        Collections.sort(allRating, (Rating o1, Rating o2) -> Integer.compare(o1.getMovie().getId(), o2.getMovie().getId()));
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(tmp))) {
-            for (Rating rating : allRating) {
-                bw.write(rating.getMovie().getId() + "," + rating.getUser().getId() + "," + rating.getRating());
-                bw.newLine();
+    public void updateRating(Rating rating) throws IOException
+    {
+        try (RandomAccessFile raf = new RandomAccessFile(RATING_SOURCE, "rw"))
+        {
+            long totalRatings = raf.length();
+            long low = 0;
+            long high = ((totalRatings - 1) / RECORD_SIZE) * RECORD_SIZE;
+            while (high >= low) //Binary search of movie ID
+            {
+                long pos = (((high + low) / 2) / RECORD_SIZE) * RECORD_SIZE;
+                raf.seek(pos);
+                int movId = raf.readInt();
+                int userId = raf.readInt();
+
+                if (rating.getMovie().getId() < movId) //We did not find the movie.
+                {
+                    high = pos - RECORD_SIZE; //We half our problem size to the upper half.
+                } else if (rating.getMovie().getId() > movId) //We did not find the movie.
+                {
+                    low = pos + RECORD_SIZE; //We half our problem size (Just the lower half)
+                } else //We found a movie match, not to search for the user:
+                {
+                    if (rating.getUser().getId() < userId) //Again we half our problem size
+                    {
+                        high = pos - RECORD_SIZE;
+                    } else if (rating.getUser().getId() > userId) //Another half sized problem
+                    {
+                        low = pos + RECORD_SIZE;
+                    } else //Last option, we found the right row:
+                    {
+                        raf.write(rating.getRating()); //Remember the to reads at line 60,61. They positioned the filepointer just at the ratings part of the current record.
+                        return; //We return from the method. We are done here. The try with resources will close the connection to the file.
+                    }
+                }
             }
         }
-        Files.copy(tmp.toPath(), new File(RATING_SOURCE).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        throw new IllegalArgumentException("Rating not found in file, can't update!"); //If we reach this point we have been searching for a non-present rating.
     }
 
-    /**
+    /** lav om
      * Removes the given rating.
      *
      * @param rating
@@ -81,65 +106,52 @@ public class RatingDAO {
         File file = new File(RATING_SOURCE);
         File temp = new File(TEMP_SOURCE);
 
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
-
-        String lineToRemove;
-
-        while ((lineToRemove = reader.readLine()) != null) {
-            if (null != lineToRemove && !lineToRemove.equalsIgnoreCase(rating.toString())) {
-                writer.write(lineToRemove + System.getProperty("line.separator"));
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(temp));
+            
+            String lineToRemove;
+            
+            while ((lineToRemove = reader.readLine()) != null) {
+                if (null != lineToRemove && !lineToRemove.equalsIgnoreCase(rating.toString())) {
+                    writer.write(lineToRemove + System.getProperty("line.separator"));
+                }
             }
+            writer.close();
         }
-        writer.close();
-        reader.close();
 
-        boolean deleted = file.delete();
-        boolean successful = temp.renameTo(file);
-        System.out.println(successful);
+        file.delete();
+        temp.renameTo(file);
+        
     }
 
-    /**
+    /** peter
      * Gets all ratings from all users.
      *
      * @return List of all ratings.
      * @throws java.io.IOException
      */
-    public List<Rating> getAllRatings() throws IOException {
-        File file = new File(RATING_SOURCE);
+    public List<Rating> getAllRatings() throws IOException
+    {
         List<Rating> allRatings = new ArrayList<>();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                try {
-                    Rating mov = createRatingFromString(line);
-                    allRatings.add(mov);
-                } catch (Exception ex) {
-                    //Do nothing
-                }
-            }
+        MovieDAO moviedao = new MovieDAO();
+        UserDAO userdao = new UserDAO();
+        byte[] all = Files.readAllBytes(new File(RATING_SOURCE).toPath()); //I get all records as binary data!
+        for (int i = 0; i < all.length; i += RECORD_SIZE)
+        {
+            int movieId = ByteBuffer.wrap(all, i, Integer.BYTES).order(ByteOrder.BIG_ENDIAN).getInt();
+            int userId = ByteBuffer.wrap(all, i + Integer.BYTES, Integer.BYTES).order(ByteOrder.BIG_ENDIAN).getInt();
+            int rating = ByteBuffer.wrap(all, i + Integer.BYTES * 2, Integer.BYTES).order(ByteOrder.BIG_ENDIAN).getInt();
+            Rating r = new Rating(moviedao.getMovie(movieId), userdao.getUser(userId), rating);
+            allRatings.add(r);
         }
+        Collections.sort(allRatings, (Rating o1, Rating o2) ->
+        {
+            int movieCompare = Integer.compare(o1.getMovie().getId(), o2.getMovie().getId());
+            return movieCompare == 0 ? Integer.compare(o1.getUser().getId(), o2.getUser().getId()) : movieCompare;
+        });
         return allRatings;
     }
-
-    private Rating createRatingFromString(String strRating) throws IOException {
-        UserDAO userDAO = new UserDAO();
-        MovieDAO movieD = new MovieDAO();
-
-        String[] columms = strRating.split(",");
-
-        int userId = Integer.parseInt(columms[0]);
-        User user = userDAO.getUser(userId);
-
-        int movieId = Integer.parseInt(columms[1]);
-        Movie movie = movieD.getMovie(movieId);
-
-        int ratingOfMovie = Integer.parseInt(columms[2]);
-        Rating rating = new Rating(movie, user, ratingOfMovie);
-        return rating;
-    }
-
+    
     /**
      * Get all ratings from a specific user.
      *
